cb2582f1b261403b744a4253c7438be86b51d907
Erick <erick.nahuelpaiva.001@gmail.com>
2022-04-20 22:44:26 -0300

    termine la practica 1-7

---
+++ variables.js
@@ -0,0 +1,23 @@
+// var + nombre de variable + = + valor literal
+
+//primer practica
+var anioActual = 2022;//int o entero o integer
+
+var nombreMascotaPreferida = "toffee"; // string o cadena de caracteres o tipo de dato palabra
+
+var estaLloviendo = false; // bool o boolean o booleano
+
+var cantBitconsPoseo = 0.000024; // float o decimal
+
+// terminamos la primer parte 
+
+// segunda practica
+document.write(`
+    <h4> año actual: ${ anioActual } </h4>
+    <h4> nombre de mi mascota preferida: ${ nombreMascotaPreferida } </h4>
+    <h4> esta lloviendo? true para si - false para no: ${ estaLloviendo } </h4>
+    <h4> mis bitcoins: ${ cantBitconsPoseo } BTC </h4>
+`);
+
+
+
